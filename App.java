cdd350cc7650851dacf74a233bf52403b35bb857
ramkumar <ramkumar.vemula7777@gmail.com>
2026-07-11 12:41:33 +0530

    Revert "new-update-2"
    This reverts commit 714ab333e6e23a8e79528e6d51d4315af3557614.

--- App.java
+++ App.java
@@ -2,7 +2,6 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("GOOD MORNING");
 		System.out.println("GIT REVERT COMMAND");
-		System.out.println("NEW feature add -2");
 		System.out.println("New Feature Add");
 	}
 }
